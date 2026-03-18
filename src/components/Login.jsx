594558a0464d4f0b0280886e220e4b9f6a67283e
Chandu <chandrasekarramella@gmail.com>
2026-03-18 23:25:08 +0530

    building logout feature

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,7 @@ const Login = () => {
 
   const [emailId, setEmailId] = useState("ratan@gmail.com");
   const [password, setPassword] = useState("Ratan@123");
+  const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -21,56 +22,65 @@ const Login = () => {
       return navigate("/");
     }
     catch (err) {
-      console.error(err)
+      setErrorMessage(err?.response?.data || "Something went wrong");
     }
   }
   return (
-    <div className="h-full flex items-center justify-center bg-base-100 mt-10">
+    <div className="flex items-center justify-center min-h-[calc(100vh-80px)] overflow-hidden">
 
       <div className="w-full max-w-md bg-base-200 p-8 rounded-2xl shadow-xl border border-base-300">
 
-        <h2 className="text-3xl font-bold text-center mb-6">Login Page</h2>
+        <h2 className="text-3xl font-bold text-center mb-6">
+          Login Page
+        </h2>
 
-        {/* Email */}
         <div className="form-control mb-4">
-          <label className="label font-medium mb-1">Email</label>
+          <label className="label">
+            <span className="label-text font-medium">Email</span>
+          </label>
           <input
             type="email"
             value={emailId}
             onChange={(e) => setEmailId(e.target.value)}
             placeholder="Enter your email"
-            className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-neutral pl-4"
+            className="p-2 input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-neutral transition-all duration-200"
           />
         </div>
 
-        {/* Password */}
         <div className="form-control mb-2">
-          <label className="label font-medium mb-1">Password</label>
+          <label className="label">
+            <span className="label-text font-medium">Password</span>
+          </label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Enter your password"
-            className="input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-neutral  pl-4"
+            className="p-2 input input-bordered w-full focus:outline-none focus:ring-2 focus:ring-neutral transition-all duration-200"
           />
         </div>
 
-        {/* Forgot Password */}
-        <div className="text-right mb-4">
+        <div className="flex items-center justify-between mb-4">
+          <p className="text-red-500 text-s font-medium">
+            {errorMessage}
+          </p>
           <a className="text-sm text-blue-500 hover:underline cursor-pointer">
             Forgot password?
           </a>
         </div>
 
-        {/* Button */}
-        <button className="btn btn-neutral w-full text-base font-semibold"
-          onClick={handleLogin}>
+        <button
+          className="btn btn-neutral w-full text-base font-semibold rounded-lg hover:scale-[1.02] transition-transform duration-200"
+          onClick={handleLogin}
+        >
           Login
         </button>
+
       </div>
     </div>
-  );
-};
+
+  )
+}
 
 
 export default Login;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,24 @@
-import { Link } from "react-router-dom"
-import { useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import axios from "axios"
+import { BASE_URL } from "../utils/constants"
+import { removeUser } from "../utils/userSlice"
 
 const NavBar = () => {
   const user  = useSelector(store => store.user)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = async ()=>{
+    try{
+      await axios.post(BASE_URL + "/logout",{},{withCredentials : true});
+      navigate("/login");
+      dispatch(removeUser());
+    }
+    catch(err){
+      console.log(err)
+    }
+  }
 
   return (
     <div className="navbar bg-base-200 shadow-sm">
@@ -39,7 +55,7 @@ const NavBar = () => {
                 </Link>
               </li>
               <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
+              <li><a onClick={handleLogout}>Logout</a></li>
             </ul>
           </div>
 
